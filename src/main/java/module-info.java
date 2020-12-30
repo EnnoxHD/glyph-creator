be8f7d33729f6c7f57a882f724ccd9712501a639
EnnoxHD <henrik.schirmacher@outlook.de>
2020-12-30 16:52:54 +0100

    Javadoc for module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,6 @@
+/**
+ * Single module for the application
+ */
 module com.github.ennoxhd.glyphcreator {
 	exports com.github.ennoxhd.glyphcreator.app;
 	exports com.github.ennoxhd.glyphcreator.ui to javafx.fxml, javafx.graphics;
