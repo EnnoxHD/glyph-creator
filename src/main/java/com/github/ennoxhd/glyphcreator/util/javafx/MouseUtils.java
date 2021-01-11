c52c5e844c6d95ecb10ed6c0e2bd14955938cc36
EnnoxHD <henrik.schirmacher@outlook.de>
2021-01-11 22:11:06 +0100

    Javadoc for MouseUtils

--- src/main/java/com/github/ennoxhd/glyphcreator/util/javafx/MouseUtils.java
+++ src/main/java/com/github/ennoxhd/glyphcreator/util/javafx/MouseUtils.java
@@ -5,8 +5,15 @@ import java.awt.Point;
 
 import javafx.geometry.Point2D;
 
+/**
+ * Contains some mouse input related utility functions.
+ */
 class MouseUtils {
 
+	/**
+	 * Gets the current position of the mouse cursor on the screen.
+	 * @return the position of the mouse cursor
+	 */
 	static Point2D getMousePosition() {
 		Point mouseLocation = MouseInfo.getPointerInfo().getLocation();
 		return new Point2D(mouseLocation.getX(), mouseLocation.getY());
