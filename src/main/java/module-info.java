ce0fb4d9f11d46c7ac5ffa7a217644e16973c99d
EnnoxHD <henrik.schirmacher@outlook.de>
2021-03-14 19:29:15 +0100

    Remove implicit module dependency java.base

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module com.github.ennoxhd.glyphcreator {
 	exports com.github.ennoxhd.glyphcreator.app;
 	exports com.github.ennoxhd.glyphcreator.ui to javafx.fxml, javafx.graphics;
 	opens com.github.ennoxhd.glyphcreator.ui to javafx.fxml;
-	requires java.base;
 	requires javafx.base;
 	requires transitive javafx.controls;
 	requires javafx.fxml;
